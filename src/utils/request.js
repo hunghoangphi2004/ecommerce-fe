4277517f08ae67109414d1947fffde22c490611c
Nguyen Hoang Phi Hung <nhph20049@gmail.com>
2026-03-05 18:30:16 +0700

    sua api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const request = async (path, options = {}) => {
     const tokenUser = localStorage.getItem("tokenUser");
